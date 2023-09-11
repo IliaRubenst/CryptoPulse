7bc83cbdf961468760c75cffb79d78f2c9d2e1a4
Vitaliy B <lilrekter@mail.ru>
2023-09-11 15:53:54 +0300

    Рефакторинг DetailView.
    Вынес сокетМенеджер в отдельную модель, повесил обсервер. Рефакторинг логики открытия и закрытия сокетов через appLifeCycle.

--- ShareProject/ShareProject/DetailViewController.swift
+++ ShareProject/ShareProject/DetailViewController.swift
@@ -7,120 +7,35 @@
 
 import UIKit
 
-class DetailViewController: UIViewController, URLSessionWebSocketDelegate {
+class DetailViewController: UIViewController {
     @IBOutlet weak var symbolLabel: UILabel!
     @IBOutlet weak var receiveDataText: UITextView!
     
+    var webSocketManager = WebSocketManager()
+    
     var symbol: String!
     var price: String!
-    
-    var objectSymbol = "name"
-    var objectPrice = "0.0$"
-    
-    private var webSocket: URLSessionWebSocketTask?
+
     var isClose = false
-    var receivedData: String? {
-        didSet {
-            receiveDataText.text = "Current price of \(objectSymbol) \n is \(objectPrice) $"
-        }
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        receiveDataText.text = "Current price of name \n is    $"
-//        symbolLabel.text = "Current coin is \(ReceivedWebData.s) price \(ReceivedWebData.p) $"
-        webSocketConnect()
-        let startConnectionBtn = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(webSocketConnect))
-        let stopConnectionBtn = UIBarButtonItem(barButtonSystemItem: .pause, target: self, action: #selector(close))
-        navigationItem.rightBarButtonItems = [startConnectionBtn, stopConnectionBtn]
-    }
-    
-    @objc func webSocketConnect() {
-        isClose = false
-//        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
-        let session = URLSession(configuration: .default, delegate: self, delegateQueue: .main)
-
-//        let coinSymbol = "ethusdt"
-        let coinSymbol = symbol.lowercased()
-        guard let url = URL(string: "wss://fstream.binance.com:443/ws/\(coinSymbol)@aggTrade") else { return }
-        webSocket = session.webSocketTask(with: url)
-        webSocket?.resume()
-//        print(url)
-//        print(coinSymbol)
-//        print(symbol.lowercased())
-    }
-    
-    func ping() {
-        webSocket?.sendPing { error in
-            if let error = error {
-                print("Ping error: \(error)")
-            }
+        updateView(symbol: symbol, price: price)
+        webSocketManager.webSocketConnect(symbol: symbol)
+        
+        webSocketManager.onValueChanged = { price, symbol in
+            self.updateView(symbol: symbol, price: price)
         }
     }
     
-    @objc func close() {
-        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
-        isClose = true
+    override func viewWillDisappear(_ animated: Bool) {
+        webSocketManager.close()
     }
     
-    func send() {
-//        if !isClose {
-//            DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
-//                self.send()
-//                self.webSocket?.send(.string("Send new request"), completionHandler: { error in
-//                    if let error = error {
-//                        print("Send error: \(error)")
-//                    }
-//                })
-//            }
-//        }
-    }
-    
-    func recieve() {
-        webSocket?.receive(completionHandler: { [weak self] result in
-            switch result {
-            case .success(let message):
-                switch message {
-                case .data(let data):
-                    print("Got data: \(data)")
-                case .string(let message):
-//                    print("Got string: \(message)")
-                    self?.receivedData = message
-                    self?.parseJSONWeb(socketString: message)
-                @unknown default:
-                    break
-                }
-            case .failure(let error):
-                print("Receive error: \(error)")
-            }
-            
-            self?.recieve()
-        })
-    }
-    
-    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
-        print("Did connect to socket")
-        ping()
-        recieve()
-        send()
-    }
-    
-    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-        print("Did close connection with reason")
-    }
-    
-    func parseJSONWeb(socketString: String) {
-        guard let socketData = socketString.data(using: String.Encoding.utf8) else { return }
-        let decoder = JSONDecoder()
-        do {
-            let decodedData = try decoder.decode(ReceivedSocketData.self, from: socketData)
-            let socketObject = ReceivedSocketData(symbol: decodedData.s, price: decodedData.p)
-            objectSymbol = decodedData.s
-            objectPrice = decodedData.p
-            print(objectPrice)
-        } catch {
-            print("Error JSON: \(error)")
+    func updateView(symbol: String, price: String) {
+        if let doublePrice = Double(price) {
+            receiveDataText.text = String(format: "Current price of \(symbol)\n is %.6f", doublePrice)
         }
     }
 }
--- ShareProject/ShareProject/WebSocketManager.swift
+++ ShareProject/ShareProject/WebSocketManager.swift
@@ -8,76 +8,93 @@
 import UIKit
 
 
-//class WebSocketManager: DetailViewController, URLSessionWebSocketDelegate {
-//    private var webSocket: URLSessionWebSocketTask?
-//
-//    var isClose = false
-//    @objc func webSocketConnect() {
-//        isClose = false
-//        let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
-//        
-//        let coinSymbol = "btcusdt@aggTrade"
-//        guard let url = URL(string: "wss://stream.binance.com:443/ws/\(coinSymbol)") else { return }
-//        print(url)
-//        webSocket = session.webSocketTask(with: url)
-//        webSocket?.resume()
-//    }
-//    
-//    func ping() {
-//        webSocket?.sendPing { error in
-//            if let error = error {
-//                print("Ping error: \(error)")
-//            }
-//        }
-//    }
-//    
-//    @objc func close() {
-//        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
-//        isClose = true
-//    }
-//    
-//    func send() {
-//        if !isClose {
-//            DispatchQueue.global().asyncAfter(deadline: .now() + 5) {
-//                self.send()
-//                self.webSocket?.send(.string("Send new message: \(Int.random(in: 0...1000))"), completionHandler: { error in
-//                    if let error = error {
-//                        print("Send error: \(error)")
-//                    }
-//                })
-//            }
-//        }
-//    }
-//    
-//    func recieve() {
-//        webSocket?.receive(completionHandler: { [weak self] result in
-//            switch result {
-//            case .success(let message):
-//                switch message {
-//                case .data(let data):
-//                    print("Got data: \(data)")
-//                case .string(let message):
-//                    print("Got string: \(message)")
-//                @unknown default:
-//                    break
-//                }
-//            case .failure(let error):
-//                print("Receive error: \(error)")
-//            }
-//            
-//            self?.recieve()
-//        })
-//    }
-//    
-//    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
-//        print("Did connect to socket")
-//        ping()
-//        recieve()
-//        send()
-//    }
-//    
-//    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
-//        print("Did close connection with reason")
-//    }
-//}
+class WebSocketManager: NSObject, URLSessionWebSocketDelegate {
+    private var webSocket: URLSessionWebSocketTask?
+    
+    var onValueChanged: ((String, String) -> ())?
+    
+    var objectSymbol = ""
+    var objectPrice = "" {
+        didSet {
+            onValueChanged?(objectPrice, objectSymbol)
+        }
+    }
+    
+    func webSocketConnect(symbol: String) {
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: .main)
+        let coinSymbol = symbol.lowercased()
+        guard let url = URL(string: "wss://stream.binance.com:443/ws/\(coinSymbol)@aggTrade") else { return }
+        print(url)
+        webSocket = session.webSocketTask(with: url)
+        webSocket?.resume()
+    }
+    
+    func ping() {
+        webSocket?.sendPing { error in
+            if let error = error {
+                print("Ping error: \(error)")
+            }
+        }
+    }
+    
+    func close() {
+        webSocket?.cancel(with: .goingAway, reason: "Demo ended".data(using: .utf8))
+    }
+    
+   
+    func recieve() {
+        webSocket?.receive(completionHandler: { [weak self] result in
+            switch result {
+            case .success(let message):
+                switch message {
+                case .data(let data):
+                    print("Got data: \(data)")
+                case .string(let message):
+                    self?.parseJSONWeb(socketString: message)
+                @unknown default:
+                    break
+                }
+            case .failure(let error):
+                print("Receive error: \(error)")
+            }
+            self?.recieve()
+        })
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        print("Did connect to socket")
+        ping()
+        recieve()
+        send()
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        print("Did close connection with reason")
+    }
+    
+    func parseJSONWeb(socketString: String) {
+        guard let socketData = socketString.data(using: String.Encoding.utf8) else { return }
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(ReceivedSocketData.self, from: socketData)
+            objectSymbol = decodedData.s
+            objectPrice = decodedData.p
+        } catch {
+            print("Error JSON: \(error)")
+        }
+    }
+    
+    func send() {
+        /*if !isClose {
+            DispatchQueue.global().asyncAfter(deadline: .now() + 5) {
+                self.send()
+                self.webSocket?.send(.string("Send new message: \(Int.random(in: 0...1000))"), completionHandler: { error in
+                    if let error = error {
+                        print("Send error: \(error)")
+                    }
+                })
+            }
+        }*/
+    }
+}
 
